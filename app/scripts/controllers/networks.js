92ef80c7de582c3a10e823dac81b9334416820db
yunlzheng <yunl.zheng@gmail.com>
2013-12-23 03:10:12 -0800

    networks list view

--- app/scripts/controllers/networks.js
+++ app/scripts/controllers/networks.js
@@ -1,6 +1,109 @@
 'use strict';
 
+function NetworksCtrl($scope, $cookieStore, $http) {
+
+	$scope.rootUrl = $cookieStore.get('rootUrl');
+	$scope.useNode = $cookieStore.get('useNode');
+
+	var httpConfig = {
+		'headers': {
+			'X-Consumer-key': $cookieStore.get('appKey'),
+			'X-Auth-Token': $cookieStore.get('accessToken')
+		}
+	};
+
+	$scope.getNetworks = function () {
+
+		var url = '/v1/networks';
+		if (!$scope.useNode) {
+			url = $scope.rootUrl + url;
+		}
+
+		$http.get(url, httpConfig).success(
+			function (data) {
+
+				$scope.networks = data;
+
+			});
+
+	}
+
+	$scope.getSubnets = function () {
+
+		var url = '/v1/subnets';
+		if (!$scope.useNode) {
+			url = $scope.rootUrl + url;
+		}
+
+		$http.get(url, httpConfig).success(
+			function (data) {
+
+				$scope.subnets = data;
+
+			});
+
+	}
+
+	$scope.getPorts = function () {
+
+		var url = '/v1/ports';
+		if (!$scope.useNode) {
+			url = $scope.rootUrl + url;
+		}
+
+		$http.get(url, httpConfig).success(
+			function (data) {
+				console.log(data);
+				$scope.ports = data;
+
+			});
+
+	}
+
+	$scope.getNats = function () {
+
+		var url = '/v1/nats';
+		if (!$scope.useNode) {
+			url = $scope.rootUrl + url;
+		}
+
+		$http.get(url, httpConfig).success(
+			function (data) {
+
+				
+				$scope.nats = data;
+
+			});
+
+	}
+
+	$scope.getSecurityGroups = function () {
+
+		var url = '/v1/security-groups';
+		if (!$scope.useNode) {
+			url = $scope.rootUrl + url;
+		}
+
+		$http.get(url, httpConfig).success(
+			function (data) {
+
+				
+				$scope.securityGroups = data;
+
+			});
+
+	}
+
+	
+	$scope.getSecurityGroups();
+	$scope.getNats();
+	$scope.getPorts();
+	$scope.getNetworks();
+	$scope.getSubnets();
+
+}
+
+NetworksCtrl.$inject = ['$scope', '$cookieStore', '$http'];
+
 angular.module('dashbordApp')
-	.controller('NetworksCtrl', function() {
-		
-	});
+	.controller('NetworksCtrl', ['$scope', '$cookieStore', '$http', NetworksCtrl]);
