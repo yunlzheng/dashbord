088212aec5474e6019f4dd5e664cf3b902a4f8db
Moo <yunl.zheng@gmail.com>
2014-01-02 12:18:36 +0800

    更新分页配置

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -63,6 +63,13 @@ function MainCtrl($rootScope, $scope, breadcrumbs, notify) {
 				}
 			];
 
+			$scope.pagerConfig = {
+				'numPerPage':10,
+				'previousText': '上一页',
+				'nextText': '下一页',
+				'firstText': '第一页',
+				'lastText': '最后一页'
+			};
 
 			$scope.select =function(menu){
 
